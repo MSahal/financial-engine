d7c5eb7c540a40708aa251a4be06cd52a7799626
Mohamed Sahal <mohamed.sahal@se.linedata.com>
2016-04-20 09:54:23 +0100

    comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,5 @@ insert into booking(booking_name) values( 'Michael');
 insert into booking(booking_name) values( 'Mario');
 insert into booking(booking_name) values( 'Michelle');
 insert into booking(booking_name) values( 'Matt');
---insert into booking(booking_name) values( 'Richard');
+insert into booking(booking_name) values( 'Richard');
 --insert into booking(booking_name) values( 'Charlie');
